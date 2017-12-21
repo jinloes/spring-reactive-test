5587b40bbf052185b9da200b0faf63f0c5e64a4d
jinloes <jinloes@vmware.com>
2017-12-20 20:55:21 -0800

    restdoc

---
+++ restdoc/src/main/java/com/jinloes/Application.java
@@ -0,0 +1,14 @@
+package com.jinloes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by jinloes on 5/16/16.
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
